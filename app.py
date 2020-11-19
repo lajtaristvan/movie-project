b12960829a598ab974fbe2fc2552814314b09ed3
Istvan Lajtar <istvan.lajtar@gmail.com>
2020-11-19 16:36:52 +0100

    final

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ Tasks:
 [x]: Decide where to store movies
 [x]: What is the format of a movie?
 [x]: Show the user the main inteface and get their input
-[]: Allow users to add movies
-[]: Show all their movies
+[x]: Allow users to add movies
+[x]: Show all their movies
 []: Find a movie
 [x]: Stop running the program when they type 'q'
 """
@@ -24,7 +24,6 @@ movie = {
     'director': ... (str)
     'year': ... (int)
 }
-
 """
 
 
@@ -35,19 +34,19 @@ def menu():
         if user_input == 'a':
             add_movie()
         elif user_input == 'l':
-            show_movies()
+            show_movies(movies)
         elif user_input == 'f':
             find_movie()
         else:
             print("Unknown command, please tr again.")
 
-        user_input = input("Enter 'a' to add a movie, 'l' to see your movie, 'f' to find a movie, and 'q' to quit: ")
+        user_input = input("\nEnter 'a' to add a movie, 'l' to see your movie, 'f' to find a movie, and 'q' to quit: ")
 
 
 def add_movie():
     name = input("Enter the movie name: ")
     director = input("Enter the movie director: ")
-    year = int(input("Enter the movie release year: "))
+    year = input("Enter the movie release year: ")
 
     movies.append({
         'name': name,
@@ -56,6 +55,34 @@ def add_movie():
     })
 
 
-menu()
+def show_movies(movies_list):
+    for movie in movies_list:
+        show_movie_details(movie)
 
-print(movies)
+
+def show_movie_details(movie):
+    print(f"Name: {movie['name']}")
+    print(f"Director: {movie['director']}")
+    print(f"Release year: {movie['year']}")
+
+
+def find_movie():
+    find_by = input("What property of the movie are you looking for? ")  # one of 'year', 'name', or 'director'
+    looking_for = input("What are you searching for? ")
+
+    found_movies = find_by_attribute(movies, looking_for, lambda x: x[find_by])
+
+    show_movies(found_movies)
+
+
+def find_by_attribute(items, expected, finder):
+    found = []
+
+    for i in items:
+        if finder(i) == expected:
+            found.append(i)
+
+    return found
+
+
+menu()
